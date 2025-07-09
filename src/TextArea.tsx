a8550dad8174cedaf7547a03ab822f222de1f9e0
Ross <berklandrc@gmail.com>
2025-07-09 20:51:30 +0200

    Add native prop support to TextArea

--- src/TextArea.tsx
+++ src/TextArea.tsx
@@ -1,17 +1,25 @@
 const TextArea = ({
-  value,
   onChange,
+  className,
+  ...props
 }: {
-  value?: string
   onChange?: (value: string) => void
-}) => {
+} & Omit<
+  React.DetailedHTMLProps<
+    React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+    HTMLTextAreaElement
+  >,
+  'onChange'
+>) => {
   return (
     <textarea
-      value={value}
       onChange={(event) => {
-        onChange(event.target.value)
+        onChange?.(event.target.value)
       }}
-      className="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      className={`block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
+        className ?? ''
+      }`}
+      {...props}
     />
   )
 }
