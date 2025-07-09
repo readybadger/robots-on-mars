c50a2f54fe79f1e6488389b31101fdcb2d940d60
Ross <berklandrc@gmail.com>
2025-07-09 20:52:00 +0200

    Add basic input/output and command button

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,27 @@ import TextArea from './TextArea'
 
 function App() {
   const [inputText, setInputText] = useState<string>('')
+  const [resultText, setResultText] = useState<string>('')
+
+  const onRunSimulation = () => {
+    setResultText(inputText)
+  }
+
   return (
-    <div className="flex justify-between w-full">
+    <div className="flex justify-between w-full gap-x-10">
       <div className="grow">
         <TextArea value={inputText} onChange={setInputText} />
       </div>
+      <button
+        onClick={onRunSimulation}
+        className="cursor-pointer self-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Run
+        <br />
+        Simulation
+      </button>
       <div className="grow">
-        <TextArea value={inputText} />
+        <TextArea value={resultText} />
       </div>
     </div>
   )
