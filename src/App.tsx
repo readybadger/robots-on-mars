15eebabaa8a0dde960556f2b3fde9f015f4f71fa
Ross <berklandrc@gmail.com>
2025-07-10 20:24:55 +0200

    Add input/output and processing, remove App styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
 import { useState } from 'react'
-import './App.css'
 import TextArea from './TextArea'
+import { runSimulation } from './lib/simulation'
+import type { Robot } from './types/robot'
+import type { Position } from './types/spatial'
+
+const positionToString = (position: Position): string => {
+  return `${position.location.x} ${position.location.y} ${position.facing}`
+}
+
+const getOutput = (robots: Robot[]) => {
+  return robots
+    .map(
+      ({ position, lost }) =>
+        `${positionToString(position)}${lost ? ' LOST' : ''}`
+    )
+    .join('\n')
+}
 
 function App() {
-  const [inputText, setInputText] = useState<string>('')
+  const [inputText, setInputText] = useState<string>(`5 3
+
+1 1 E
+RFRFRFRF
+
+3 2 N
+FRRFLLFFRRFLL
+
+0 3 W
+LLFFFLFLFL
+  `)
   const [resultText, setResultText] = useState<string>('')
 
   const onRunSimulation = () => {
-    setResultText(inputText)
+    const { robots } = runSimulation(inputText)
+    setResultText(getOutput(robots))
   }
 
   return (
     <div className="flex justify-between w-full gap-x-10">
       <div className="grow">
-        <TextArea value={inputText} onChange={setInputText} />
+        <TextArea value={inputText} onChange={setInputText} rows={20} />
       </div>
       <button
         onClick={onRunSimulation}
@@ -24,7 +50,7 @@ function App() {
         Simulation
       </button>
       <div className="grow">
-        <TextArea value={resultText} />
+        <TextArea value={resultText} rows={20} />
       </div>
     </div>
   )
